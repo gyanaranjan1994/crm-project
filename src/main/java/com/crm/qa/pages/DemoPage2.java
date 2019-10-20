3d4135fe2fa94f5e4b8828f11c43592cc95b44c9
ag17431 <ag17431@L904Q8S2.us.ad.wellpoint.com>
2019-10-20 22:08:34 +0530

    new functionalty add

--- src/main/java/com/crm/qa/pages/DemoPage2.java
+++ src/main/java/com/crm/qa/pages/DemoPage2.java
@@ -2,11 +2,23 @@ package com.crm.qa.pages;
 
 public class DemoPage2 {
 	public void demo1() {
-		
+		System.out.println("hello");
+
 	}
-	public void demo2()
-	{
-		
+
+	public void demo2() {
+		System.out.println("hello");
+
+	}
+
+	public void demo3() {
+		System.out.println("hello");
+
+	}
+
+	public void demo4() {
+		System.out.println("hello");
+
 	}
 
 }
